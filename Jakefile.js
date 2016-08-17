ead3963af34e81d7ab9313a79fa7afbfaa87e910
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-17 17:17:28 +0300

    Check manually if the correct Node version is installed

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,19 @@
 (function(){
 	"use strict";
+
+
 	desc("Default Build");
 	task("default", function(){
 		console.log("\n\nBUILD OK");
+	});
+
+	desc("Checks if the correct Node version installed");
+	task("checkVersion", function(){
+		var EXPECTED_VERSION = require("./package.json").engines.node;
+		var actualVersion = process.version;
+
+		if ("v" + EXPECTED_VERSION !== actualVersion) {
+			fail("You've installed an incorrect Node version: " + actualVersion + '. The expected Node version is: ' + EXPECTED_VERSION);
+		}
 	});
 }());
