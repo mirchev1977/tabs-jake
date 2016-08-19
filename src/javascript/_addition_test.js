dcaca5e06ae4385db070c450991c0f1c0cf13163
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-19 11:10:46 +0300

    added subtraction and tested it

--- src/javascript/_addition_test.js
+++ src/javascript/_addition_test.js
@@ -1,24 +1,17 @@
 (function() {
 	"use strict";
 
-	var addition = require("./addition.js");
 	var assert = require("./assert.js");
+	var addition = require("./addition.js");
 
 	describe("Addition", function() {
 
 		it("adds positive numbers", function() {
-			// assertEqual(addition.add(3, 4), 7);
 			assert.equal(addition.add(3, 4), 7);
 		});
 
 		it("uses IEEE 754 floating point", function() {
-			// assertEqual(addition.add(0.1, 0.2), 0.30000000000000004);
 			assert.equal(addition.add(0.1, 0.2), 0.30000000000000004);
 		});
-
-		// function assertEqual(actual, expected) {
-		// 	if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
-		// }
-
 	});
 }());
