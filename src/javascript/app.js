43e366a1930d8cccfceba8036672f606b8f96d4d
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-19 17:14:50 +0300

    created tabsInitialize function in tabs.js

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -3,7 +3,9 @@
 	
 	var addition = require("./addition.js");
 	var subtraction = require("./subtraction.js");
+	var tabs = require("./tabs.js");
 
+	tabs.tabsInitialize();
 	console.log("42 + 13 = " + addition.add(42, 13));
 	console.log("100 - 50 = " + subtraction.subtract(100, 50));
 }());
---
+++ src/javascript/tabs.js
@@ -0,0 +1,9 @@
+(function(){
+	"use strict";
+	
+	exports.tabsInitialize = function tabsInitialize(){
+		var firstTab = document.getElementById("tab1");
+
+		console.log(firstTab);
+	}
+}());
