afa092262c468976ff6452b365011dfa19d26132
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-19 10:20:07 +0300

    isolated chai-3.5.0.js behind the assert.js wrapper

---
+++ src/javascript/assert.js
@@ -0,0 +1,6 @@
+(function(){
+	"use strict";
+	var assert = require("../vendors/chai-3.5.0.js").assert;
+
+	module.exports = assert;
+}());
--- src/javascript/test.js
+++ src/javascript/test.js
@@ -2,20 +2,23 @@
 	"use strict";
 
 	var addition = require("./addition.js");
+	var assert = require("./assert.js");
 
 	describe("Addition", function() {
 
 		it("adds positive numbers", function() {
-			assertEqual(addition.add(3, 4), 7);
+			// assertEqual(addition.add(3, 4), 7);
+			assert.equal(addition.add(3, 4), 7);
 		});
 
 		it("uses IEEE 754 floating point", function() {
-			assertEqual(addition.add(0.1, 0.2), 0.30000000000000004);
+			// assertEqual(addition.add(0.1, 0.2), 0.30000000000000004);
+			assert.equal(addition.add(0.1, 0.2), 0.30000000000000004);
 		});
 
-		function assertEqual(actual, expected) {
-			if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
-		}
+		// function assertEqual(actual, expected) {
+		// 	if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
+		// }
 
 	});
 }());
