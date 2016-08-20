829a1118c8057134ca9ff5cda8df262c76e6338f
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-20 14:25:39 +0300

    append nav and content to container in the test block

--- src/javascript/tabsTests.js
+++ src/javascript/tabsTests.js
@@ -11,7 +11,12 @@
 		});
 
 		it("creates three tabs and three containers with text inside", function(){
-			var nav = '<nav>' +
+			var container = document.createElement('div');
+			container.setAttribute('class', 'container');
+			document.body.appendChild(container);
+
+			var nav = document.createElement('nav');
+			nav.innerHTML = '<nav>' +
 				'<ul class="tabs">' +
 					'<li id="tab1">Tab 1</li>' +
 					'<li id="tab2" class="active">Tab 2</li>' +
@@ -19,8 +24,9 @@
 				'</ul>' +
 			'</nav>';
 
-			var content = 
-			'<article class="content">' +
+			var content = document.createElement('article');
+			content.setAttribute('class', 'content');
+			content.innerHTML = 
 				'<div id="content1">' +
 					'<h2>Content 1</h2>' +
 					'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.' + ' Nam sit amet vestibulum quam, quis fermentum justo. Proin ultricies ex id lectus iaculis, blandit porttitor nulla vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt leo non gravida scelerisque. Aliquam neque ipsum, ultricies ac mollis quis, vehicula vitae lorem. Mauris ornare urna a aliquam congue. Donec sed tortor egestas sapien volutpat pulvinar at nec erat. Morbi ut tellus in velit tincidunt tincidunt. Mauris vel sapien mauris.</p>' +
@@ -32,8 +38,13 @@
 				'<div id="content3">' +
 					'<h2>Content 3</h2>' +
 					'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet vestibulum quam, quis fermentum justo. Proin ultricies ex id lectus iaculis, blandit porttitor nulla vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt leo non gravida scelerisque. Aliquam neque ipsum, ultricies ac mollis quis, vehicula vitae lorem. Mauris ornare urna a aliquam congue. Donec sed tortor egestas sapien volutpat pulvinar at nec erat. Morbi ut tellus in velit tincidunt tincidunt. Mauris vel sapien mauris.</p>' +
-				'</div>' +
-			'</article>';
+				'</div>';
+
+			container.appendChild(nav);
+			container.appendChild(content);
+
+			var cont = document.querySelector('body .container');
+			console.log(cont);
 		});
 	});
 
