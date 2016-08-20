807e81c57cd7a6e5f95d75db27228520f8a5d4ce
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-20 10:59:11 +0300

    check if tabInitialize function exists

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -2,8 +2,6 @@
 	"use strict";
 	
 	exports.tabsInitialize = function tabsInitialize(){
-		var firstTab = document.getElementById("tab1");
-
-		return firstTab;
+		
 	};
 }());
--- src/javascript/tabsTests.js
+++ src/javascript/tabsTests.js
@@ -4,9 +4,10 @@
 	var assert = require('./assert.js');
 	var tabs = require('./tabs.js');
 
-	describe("Tabs", function(){
-		it("is initialized on index load", function(){
-			tabs.tabsInitialize();
+	describe("tabs", function(){
+
+		it("checks if tabsInitialize function exists", function(){
+
 		});
 	});
 
