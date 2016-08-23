ea818e61a3c188480503e57e274ae9d40f9216f5
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-23 11:37:55 +0300

    generate tabs from texts

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -1,17 +1,13 @@
 (function(){
 	"use strict";
 	
-	exports.tabsInitialize = function tabsInitialize(tabsNumber){
+	exports.tabsInitialize = function tabsInitialize(texts){
 		var container = document.createElement('div');
 		container.setAttribute('class', 'container');
 		document.body.appendChild(container);
 
 		var nav = document.createElement('nav');
-		nav.innerHTML = '<nav>' +
-			'<ul class="tabs">' +
-				generateTabsHtml(tabsNumber);
-			'</ul>' +
-		'</nav>';
+		nav.innerHTML = generateTabsHtml(texts.length);
 
 		var content = document.createElement('article');
 		content.setAttribute('class', 'content');
@@ -35,10 +31,12 @@
 	};
 
 	function generateTabsHtml(tabsNumber){
-		var html = '';
+		var html = '<nav><ul class="tabs">';
 		for (var i = 1; i < tabsNumber + 1; i++) {
 			html += '<li id = "tab' + i + '">Tab ' + i + '</li>';
 		}
+
+		html += '</ul></nav>';
 
 		return html;
 	}
--- src/javascript/tabsTests.js
+++ src/javascript/tabsTests.js
@@ -8,8 +8,13 @@
 
 		styleTabs();
 
-		it("creates three tabs and three containers with text inside", function(){
-			tabs.tabsInitialize(3);
+		it("creates tabs number equaling number of texts we want to display", function(){
+			var texts = [
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' + ' Nam sit amet vestibulum quam, quis fermentum justo. Proin ultricies ex id lectus iaculis, blandit porttitor nulla vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt leo non gravida scelerisque. Aliquam neque ipsum, ultricies ac mollis quis, vehicula vitae lorem. Mauris ornare urna a aliquam congue. Donec sed tortor egestas sapien volutpat pulvinar at nec erat. Morbi ut tellus in velit tincidunt tincidunt. Mauris vel sapien mauris.',
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' + ' Nam sit amet vestibulum quam, quis fermentum justo. Proin ultricies ex id lectus iaculis, blandit porttitor nulla vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt leo non gravida scelerisque. Aliquam neque ipsum, ultricies ac mollis quis, vehicula vitae lorem. Mauris ornare urna a aliquam congue. Donec sed tortor egestas sapien volutpat pulvinar at nec erat. Morbi ut tellus in velit tincidunt tincidunt. Mauris vel sapien mauris.',
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' + ' Nam sit amet vestibulum quam, quis fermentum justo. Proin ultricies ex id lectus iaculis, blandit porttitor nulla vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt leo non gravida scelerisque. Aliquam neque ipsum, ultricies ac mollis quis, vehicula vitae lorem. Mauris ornare urna a aliquam congue. Donec sed tortor egestas sapien volutpat pulvinar at nec erat. Morbi ut tellus in velit tincidunt tincidunt. Mauris vel sapien mauris.'
+			];
+			tabs.tabsInitialize(texts);
 		});
 	});
 
