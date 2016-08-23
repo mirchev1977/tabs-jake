71dadefc6a5e0c33eda2245fb3bb636d19ed2ac2
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-23 12:56:05 +0300

    renamed activeTab to defaultTab

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -1,27 +1,27 @@
 (function(){
 	"use strict";
 	
-	exports.tabsInitialize = function tabsInitialize(texts, activeTab){
+	exports.tabsInitialize = function tabsInitialize(texts, defaultTab){
 		var container = document.createElement('div');
 		container.setAttribute('class', 'container');
 		document.body.appendChild(container);
 
 		var nav = document.createElement('nav');
-		nav.innerHTML = generateTabsHtml(texts.length, activeTab);
+		nav.innerHTML = generateTabsHtml(texts.length, defaultTab);
 
 		var content = document.createElement('article');
 		content.setAttribute('class', 'content');
-		content.innerHTML = generateContentSections(texts, activeTab);
+		content.innerHTML = generateContentSections(texts, defaultTab);
 		
 		
 		container.appendChild(nav);
 		container.appendChild(content);
 	};
 
-	function generateTabsHtml(tabsNumber, activeTab){
+	function generateTabsHtml(tabsNumber, defaultTab){
 		var html = '<nav><ul class="tabs">';
 		for (var i = 1; i < tabsNumber + 1; i++) {
-			if(i === activeTab){
+			if(i === defaultTab){
 				html += '<li id = "tab' + i + '" class= "active">Tab ' + i + '</li>';
 			} else {
 				html += '<li id = "tab' + i + '">Tab ' + i + '</li>';
@@ -33,10 +33,10 @@
 		return html;
 	}
 
-	function generateContentSections(texts, activeTab){
+	function generateContentSections(texts, defaultTab){
 		var html = '';
 		texts.forEach( function(text, index) {
-			if(activeTab === (parseInt(index) + 1)){
+			if(defaultTab === (parseInt(index) + 1)){
 				html += '<div id="content' + (parseInt(index) + 1) + '" class="active"><h2>Content ' + (parseInt(index) + 1) + '</h2>';
 			} else {
 				html += '<div id="content' + (parseInt(index) + 1) + '"><h2>Content ' + (parseInt(index) + 1) + '</h2>';
